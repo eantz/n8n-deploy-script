30c1bf9d5aa4b79e77fc130931b74a182a28fec0
Destiya <e_antz@yahoo.com>
2025-06-30 11:06:18 +0700

    fix n8n-compose, add dockerfile for custom certificates

---
+++ custom-ecs-deployment/aws-ca.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+REGION_NAME="ap-southeast-1"
+CERTIFICATE_NAME="$REGION_NAME-bundle.pem"
+OUTPUT_DIR="opt/custom-certificates"
+
+mkdir -p $OUTPUT_DIR
+
+if test -f "$CERTIFICATE_NAME" 
+then
+  echo "source certificate already exists, skipping download."
+else
+  wget https://truststore.pki.rds.amazonaws.com/$REGION_NAME/$CERTIFICATE_NAME
+fi
+
+
+# Initialize counter
+CERT_COUNT=0
+
+# Read and split certificates
+awk -v outdir="$OUTPUT_DIR" '
+  /-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/ {
+    print >> outdir "/cert" cert_index ".pem"
+    if (/-----END CERTIFICATE-----/) {
+      cert_index++
+    }
+  }
+' cert_index=0 "$CERTIFICATE_NAME"
+
+echo "Certificates have been saved in the '$OUTPUT_DIR' directory."
+
+
+
+
+
